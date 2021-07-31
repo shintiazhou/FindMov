f5b3b92f7ec2a2842bf2d0637e176e69eaac88bb
shintia zhou <shintiazhouu28@gmail.com>
2021-07-31 14:01:23 -0700

    update single movie info component

--- components/SingleMovieContent.js
+++ components/SingleMovieContent.js
@@ -1,20 +1,60 @@
 import { useEffect, useState } from "react"
-import { img_300, loadingImg, unavailable } from "../config/imgConfig"
+import { img_300, img_780, loadingImg, unavailable } from "../config/imgConfig"
 import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import StarIcon from '@material-ui/icons/Star';
 
 const useStyles = makeStyles((theme) => ({
+    loading: {
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
     container: {
         backgroundColor: "#171717",
-        height: "70%",
-        width: "70%",
+        height: "80%",
+        width: "80%",
         position: "relative",
         [theme.breakpoints.up('lg')]: {
             width: "60%",
         },
+        overflowY: "scroll",
+        overflowX: "hidden",
+
+    },
+    content: {
+        textAlign: "center",
+        width: "100%",
+        height: "100%",
+        padding: "30px",
+        position: "absolute",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        [theme.breakpoints.up('md')]: {
+            flexDirection: "row",
+            alignItems: "flex-start",
+            textAlign: "left"
+        },
+    },
+    background: {
+        opacity: ".05",
+        backgroundRepeat: "no-repeat",
+        position: "absolute",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        width: "100%",
+        height: "100%"
     },
     imgContainer: {
-        margin: "20px auto",
-        width: "40%",
+        width: "30%",
+        [theme.breakpoints.down('xs')]: {
+            width: "60%",
+        },
+        [theme.breakpoints.up('md')]: {
+            marginRight: "30px"
+        },
     },
     overlay: {
         backgroundImage: "linear-gradient(rgba(23,23,23,0) , rgba(23,23,23,1))",
@@ -26,6 +66,35 @@ const useStyles = makeStyles((theme) => ({
     img: {
         border: "3px solid white",
         boxShadow: " 0px 10px 18px #000000",
+    },
+    title: {
+        fontSize: "28px",
+        marginBottom: "5px"
+
+    },
+    year: {
+        fontWeight: "400",
+        opacity: ".8"
+    },
+    genres: {
+        opacity: ".8",
+        fontSize: "15px",
+        marginTop: "5px",
+    },
+    info: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    rating: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingBottom: "10px",
+        marginTop: "10px",
+        borderBottom: "1px solid gray",
+        "& > *": {
+            margin: "2px"
+        }
     }
 
 }));
@@ -35,7 +104,7 @@ function SingleMovieContent(props) {
     const [content, setContent] = useState({})
     const [image, setImage] = useState(loadingImg)
 
-    setTimeout(() => { setImage(content ? `${img_300}${content.poster_path}` : unavailable) }, 500)
+    setTimeout(() => { setImage(content.id ? `${img_300 + content.poster_path}` : unavailable) }, 500)
 
     useEffect(() => {
         if (props.arr) {
@@ -46,20 +115,71 @@ function SingleMovieContent(props) {
             }
             fetchApi()
         }
-
-    }, [props])
+        return () => setContent({})
+    }, [])
     console.log(content)
+
+    const year = content.id && (content.release_date ? content.release_date.slice(0, 4) : content.first_air_date.slice(0, 4))
+
+    const duration = () => {
+        const runtime = content.runtime;
+        let hour = Math.round(runtime / 60);
+        let minutes = Math.round((runtime / 60 - hour) * 60);
+
+        return hour + "h " + minutes + "m"
+    }
     return (
-        content &&
         <div className={classes.container}>
-            <div className={classes.imgContainer}>
-                <img
-                    className={classes.img}
-                    width="100%"
-                    src={`${image}`}
-                    alt={`${content.title}`} />
-            </div>
-            <h1></h1>
+            {content.id ? (
+                <>
+                    <div style={{ backgroundImage: `url(${img_780 + content.backdrop_path})` }}
+                        className={classes.background}></div>
+
+                    <div className={classes.content}>
+
+                        <div className={classes.imgContainer}>
+                            <img
+                                className={classes.img}
+                                width="100%"
+                                src={`${image}`}
+                                alt={`${content.title}`} />
+                        </div>
+
+                        <div className={classes.info} >
+                            <h1 className={classes.title}>
+                                {content.title ? content.title : content.name}
+
+                                <span className={classes.year}>
+                                    {" "}({year})
+                                </span>
+                            </h1>
+                            <span>{duration()}</span>
+                            <div className={classes.genres}>
+                                {content.genres.map((genre, i) => i == content.genres.length - 1 ? genre.name :
+                                    genre.name + " , ")}
+                            </div>
+                            <div className={classes.rating}>
+                                <StarIcon />
+                                <span>{content.vote_average}</span>
+
+                                <span style={{ opacity: ".5" }}>/10</span>
+
+                            </div>
+                            <div >
+                                <h2>Overview</h2>
+                                <p>{content.overview}</p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </>) :
+                <div className={classes.loading}>
+                    <CircularProgress color="secondary" />
+                </div>
+
+            }
         </div>
     )
 }
--- config/imgConfig.js
+++ config/imgConfig.js
@@ -1,7 +1,7 @@
 //image sizes for tmdb
 export const img_300 = "https://image.tmdb.org/t/p/w300";
 export const img_500 = "https://image.tmdb.org/t/p/w500";
-export const img_1280 = "https://image.tmdb.org/t/p/w1280";
+export const img_780 = "https://image.tmdb.org/t/p/w780";
 
 // contentModal and singleContent
 export const unavailable =
